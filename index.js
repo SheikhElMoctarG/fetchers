20ab908c4bbf2960f0c1bcb5ba3b4c4b4557a9fb
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-08-31 18:27:01 -0700

    added files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const Express = require("express")
-const app = Express()
-const fetch = require("node-fetch")
+const express = require("express")
+const app = express()
+const fetch = require('node-fetch')
+
 setInterval(() => {
     fetch('https://jdhfnfjnfj.herokuapp.com/').then(res => console.log(`response-ok: ${res.ok}, status: ${res.status}`))
         .catch(err => console.error(`Error occured: ${err}`))
