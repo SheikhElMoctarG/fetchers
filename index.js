83546c8f61f147f612c5a98906b0d1c021e11778
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-08-31 18:11:04 -0700

    added files

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ setInterval(() => {
     fetch('https://all-news-bot.herokuapp.com/').then(res => console.log(`response-ok: ${res.ok}, status: ${res.status}`))
     .catch(err => console.error(`Error occured: ${err}`))
 }, 15000);
+app.get("/",(req,res)=>{
+    res.send("connected")
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log("concted in port "+PORT)
